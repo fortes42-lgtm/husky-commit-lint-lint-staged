f9a7bd948978bb52a790eb391ebc9bc2471323b9
fortes42 <forteslv42@gmail.com>
2025-12-03 15:10:28 +0900

    chore: api 연결하기(#7)

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-export default { 
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-pattern': [/^(feat|fix|chore|docs|style|refactor|test|perf): .+\(#\d+\)$/, 'i'],
-    'header-max-length': [72, 'always']
-  }
-};
